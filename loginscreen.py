d28a640345f57a9a86003b1cd6555af75343927a
ebertlion <eberlion@fh-brandenburg.de>
2015-10-09 15:39:33 +0200

    Update
    Habe nun das Label hinzugefügt und einzig und allein der
    Registrier-Button muss noch verändert werden. Denke ich muss das ganze
    hier nochmal mit einer .kv schreiben, da das hier sehr unkompatibel für
    den gesamten Code am Ende ist.

--- loginscreen.py
+++ loginscreen.py
@@ -3,6 +3,8 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
 
 class LogInApp(App):
@@ -11,10 +13,11 @@ class LogInApp(App):
         layout = GridLayout(cols=2, row_force_default=True, row_default_height=30, pos_hint={'center_x':.5, 'center_y':.5}, size_hint=(None, None))
         layout.bind(minimum_size = layout.setter('size'))
         layout.bind(height = layout.setter('top'))
-        layout.add_widget(Button(text="Benutzername:", size_hint_x=None, width=120))
-        layout.add_widget(TextInput(size_hint_x=None, width=200))
-        layout.add_widget(Button(text="Kennwort:", size_hint_x=None, width=120))
-        layout.add_widget(TextInput(size_hint_x=None, width=200, password=True))
+        layout.add_widget(Label(text="Benutzername:", size_hint_x=None, width=120))
+        layout.add_widget(TextInput(id = "name_input", size_hint_x=None, width=200, multiline=False))
+        layout.add_widget(Label(text="Kennwort:", size_hint_x=None, width=120))
+        layout.add_widget(TextInput(id= "pass_input", size_hint_x=None, width=200, password=True, multiline=False))
+        layout.add_widget(Button(text="Registrieren"))
         return layout
 
 if __name__ == '__main__':
