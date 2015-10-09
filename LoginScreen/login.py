4ead883e8c0ae69caf8fbbc4f5de8ca9dc14ecfc
ebertlion <eberlion@fh-brandenburg.de>
2015-10-10 00:23:00 +0200

    Update Login Screen
    Vielleicht gehe ich euch schon auf den Sack :laughing:
    
    Hier geupdated mit ScreenManager -> 1 / 2 Button funktioniert, einfach
    mal ausprobieren, dann wisst ihr was ich meine.
    
    Mich stört die Größe des ganzen noch, das muss irgendwie skaliert
    werden, hab noch nicht rausgefunden wie, aber das wird bestimmt schon!

--- LoginScreen/login.py
+++ LoginScreen/login.py
@@ -1,18 +1,87 @@
 __author__ = 'Lion'
 
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.widget import Widget
-from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.lang import Builder
 
-class LoginScreen(Widget):
+Builder.load_string("""
+<LoginScreen>
+    f_username: username
+    f_password: password
+    GridLayout:
+        size:120,40
+        rows:3
+        cols:2
+        padding:5
+        spacing:5
+        Label:
+            text: 'Benutzername:'
+        TextInput:
+            id: username
+            multiline: False
+            write_tab: False
+        Label:
+            text: 'Passwort:'
+        TextInput:
+            id: password
+            password: True
+            multiline: False
+            write_tab: False
+        Button:
+            text: 'Registrieren..'
+            on_press: root.manager.current = 'register'
+        Button:
+            text: 'Login'
+
+<RegisterScreen>
+    f_username: username
+    f_password: password
+    f_passwordcheck: passwordcheck
+    GridLayout:
+        rows: 4
+        cols: 2
+        padding: 5
+        spacing: 5
+        Label:
+            text: 'Waehlen Sie ihren Benutzernamen:'
+        TextInput:
+            id: username
+            multiline: False
+            write_tab: False
+        Label:
+            text: 'Waehlen Sie ihr Passwort:'
+        TextInput:
+            id: password
+            password: True
+            multiline: False
+            write_tab: False
+        Label:
+            text: 'Passwort erneut eingeben:'
+        TextInput:
+            id: passwordcheck
+            password: True
+            multiline: False
+            write_tab: False
+        Button:
+            text: 'Login..'
+            on_press: root.manager.current = 'login'
+        Button:
+            text: 'Registrieren'
+""")
+
+class LoginScreen(Screen):
     pass
+
+class RegisterScreen(Screen):
+    pass
+
+sm = ScreenManager()
+sm.add_widget(LoginScreen(name='login'))
+sm.add_widget(RegisterScreen(name='register'))
 
 class LoginApp(App):
     def build(self):
-        return LoginScreen()
+        return sm
 
 if __name__ == '__main__':
     LoginApp().run()
