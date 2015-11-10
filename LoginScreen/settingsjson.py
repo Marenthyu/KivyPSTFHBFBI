b36fb8d2ce63cb5cbb80863dccaa60d2deb2da97
ebertlion <eberlion@fh-brandenburg.de>
2015-11-10 11:04:58 +0100

    Update

--- LoginScreen/settingsjson.py
+++ LoginScreen/settingsjson.py
@@ -2,7 +2,7 @@ import json
 
 settings_json= json.dumps([
     {'type': 'bool',
-     'title': 'Musik Minstellung',
+     'title': 'Musik Einstellung',
      'desc': 'Entscheide ob du die Musik hoeren willst oder nicht.',
      'section': 'einstellungen',
      'key': 'ton'}])
