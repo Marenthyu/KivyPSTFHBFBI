41de89a25059eb89c611bfe70bda9bf3ec2c2eef
MonteDes <michaeli@fh-brandenburg.de>
2015-11-12 13:56:40 +0100

    Spielfeld - funktionsfähig
    .

--- LayoutGame_decXdec.py
+++ LayoutGame_decXdec.py
@@ -25,26 +25,26 @@ class Pitch(App):
                             GridLayout.height = s
                             GridLayout.width = s
 
-                wsr = [[Button(background_normal='Wasser.jpg', background_down='Schiff.jpg', background_disabled_normal='Schiff.jpg') for x in range(11)] for y in range(11)]
-                print(wsr[4][7])
+                wsr = [[Button() for x in range(11)] for y in range(11)]
 
+                wsr[0][0] = Image(source='Weiss.png')
 
-                layout.add_widget(Image(source='Weiss.png'))
                 for i in range (1,11):
-                    wsr[0][i] =Button(background_normal='Numbers/' + str(i) + '.jpg', background_down='Numbers/' + str(i) + 'C.jpg')
-                    layout.add_widget(wsr[0][i])
+                    wsr[0][i] = Button(background_normal='Numbers/' + str(i) + '.jpg', background_down='Numbers/' + str(i) + 'C.jpg')
+
 
                 for j in range(1,11):
-                    wsr[j][0] =Button(background_normal='Letters/' + str(j) + '.jpg', background_down='Letters/' + str(j) + 'C.jpg')
-                    layout.add_widget(Button(background_normal='Letters/' + str(j) +'.jpg', background_down='Letters/' + str(j) + 'C.jpg'))
-                for k in range (1,10):
-                    wsr[k][k] = Button(background_normal='Wasser.jpg', background_down='Wasser.jpg')
-                    layout.add_widget(wsr[k][k])
+                    wsr[j][0] = Button(background_normal='Letters/' + str(j) + '.jpg', background_down='Letters/' + str(j) + 'C.jpg')
 
 
+                for k in range (1,11):
+                    for l in range (1,11):
+                        wsr[l][k] = Button(background_normal='Wasser.jpg', background_down='Schiff.jpg')
 
 
-
+                for m in range (0,11):
+                    for n in range (0,11):
+                        layout.add_widget(wsr[m][n])
 
                 return layout
 
