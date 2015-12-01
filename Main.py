d71d09fdad97f1c5be71d251fcbeeba040b12947
MonteDes <michaeli@fh-brandenburg.de>
2015-12-01 12:49:15 +0100

    Popup - Beispiel

--- Main.py
+++ Main.py
@@ -1,13 +1,22 @@
 __author__ = 'Bene'
 from kivy.app import App
-import LayoutGame_decXdec
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 
 class Main(App):
     def build(self):
-        player = LayoutGame_decXdec.Pitch().run()
+        self.won()
+        self.lost()
 
+    def won(self):
+        print('won')
+        popup = Popup(title="Won!", content=Label(text="Congratulations! \n You have won the game."))
+        popup.open()
 
+    def lost(self):
 
+        popup2 = Popup(title="Lost!", content=Label(text="You have not hit a ship. \n You have lost. Try again."), size_hint=(0.8,0.2))
+        popup2.open()
 
 if __name__ == '__main__':
     Main().run()
